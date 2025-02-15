436e91536d37b2a2c55af4d70c70eec8e626ba5a
Jaifi Niel Abuan <abuanjaifiniel_bscs@plmun.edu.ph>
2025-02-15 20:26:38 +0800

    Submitting OBJPROG Week 04 - Session 01 - Exercise 01

--- BooleanExpressionsDemo.java
+++ BooleanExpressionsDemo.java
@@ -2,23 +2,21 @@ public class BooleanExpressionsDemo {
     public static void main(String[] args) {
         boolean isJavaFun = true;
         boolean isHomeworkTiring = false;
-
-        char a = 'a';
-        char b = 'b';
-        boolean isAGreaterThanB = (a > b);
-        boolean areAAndBEqual = (a == b);
-        boolean bothConditions = (isJavaFun && (a < b));
-        boolean eitherCondition = (isJavaFun || isHomeworkTiring);
-        boolean logicalNot = !isHomeworkTiring;
-        boolean exclusiveOr = (isJavaFun ^ isHomeworkTiring);
-
+        int a = 15;
+        int b = 20;
+        boolean isAGreater = a > b;
+        boolean areEqual = a == b;
+        boolean bothTrue = isJavaFun && (a < b);
+        boolean eitherTrue = isJavaFun || isHomeworkTiring;
+        boolean notTrue =!isHomeworkTiring;
+        boolean exclusiveOr = isJavaFun ^ isHomeworkTiring;
         System.out.println("Is Java fun? " + isJavaFun);
         System.out.println("Is homework tiring? " + isHomeworkTiring);
-        System.out.println("Is 'a' greater than 'b'? " + isAGreaterThanB);
-        System.out.println("Are 'a' and 'b' equal? " + areAAndBEqual);
-        System.out.println("Both conditions (isJavaFun && a < b): " + bothConditions);
-        System.out.println("Either condition (isJavaFun || isHomeworkTiring): " + eitherCondition);
-        System.out.println("Logical NOT of isHomeworkTiring: " + logicalNot);
+        System.out.println("Is 'a' greater than 'b'? " + isAGreater);
+        System.out.println("Are 'a' and 'b' equal? " + areEqual);
+        System.out.println("Both conditions (isJavaFun && a<b): " + bothTrue);
+        System.out.println("Either condition (isJavaFun || isHomeworkTiring): " + eitherTrue);
+        System.out.println("Logical NOT of isHomeworkTiring: " + notTrue);
         System.out.println("Exclusive OR (isJavaFun ^ isHomeworkTiring): " + exclusiveOr);
     }
 }
